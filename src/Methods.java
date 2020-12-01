f312a2174b1c449d3f3145beb9d842bbfe27d766
nnanthini <nanthinin92@gmail.com>
2020-12-01 15:51:03 -0600

    Basic Java

--- src/Methods.java
+++ src/Methods.java
@@ -1,19 +1,52 @@
 
 public class Methods {
+	// User defined methods
+	// Predefined methods
+	// E.g. int x = (int) (Math.random()*4); generates a random number between 0-3
+	// Math.random() - generates random number btwn 0.0-0.9 in double
+
 	public static void main(String[] args) {
 		Methods m = new Methods();
-		m.display();
+		for (int i = 0; i < 10; i++)
+			m.display();
+		go();
+		m.go();
+		m.go1();
+		m.go2();
 	}
-	
+
 	// access modifier, return type, method name (parameter list) {
 	// method body/definition }
-	
-	
-	public void display(/*Parameter variables can be declared here but not initialized*/) { //declaration of method 
+
+	public void display(/* Parameter variables can be declared here but not initialized */) { // declaration of method
 		// what function performs is the definition of method
+		System.out.println(Math.random() * 4);
+		System.out.println((int) (Math.random() * 4));
+		/*
+		 * Typecasting reduce type to a smaller type
+		 */
 	}
 
-
-	
 	// block of statements that perform a particular function/behavior
+	//non-static can call other non-static methods directly
+	//static methods can call static methods directly
+	//non-static can call non-static directly
+	//static cant call non-static directly, needs to be called from object of class
+	//non-static can call static directly
+	// All methods cant be static because it violates OO 
+	// Reason why MAIN method is static -> JVM can call without creating a object
+	public static void go() {
+		//go1();
+		System.out.println("Inside go");
+	}
+
+	public void go1() {
+		go();
+		System.out.println("Inside go1");
+	}
+
+	public void go2() {
+		System.out.println("Inside go2");
+	}
+
 }
--- src/Variables.java
+++ src/Variables.java
@@ -6,7 +6,7 @@ public class Variables {
 	 * 2. Local
 	 * 3. Class */
 	int i = 1; // Instance or global variable (can be accessed anywhere inside class)
-	
+	static int sint = 10; //Class variables (shared across all instances of the class)
 	public void localVariableMethod() {
 		int j = 34; //Local variable (only within this function)
 	}
@@ -15,13 +15,16 @@ public class Variables {
 		Variables v = new Variables();
 		v.i = 10;
 		System.out.println(v.i);
+		System.out.println(v.sint++);
 		
 		Variables v1 = new Variables();
 		v1.i++;
 		System.out.println(v1.i);
+		System.out.println(v1.sint++);
 		
 		Variables v2=new Variables();
 		System.out.println(v2.i);
-		//v.i, v1.i, v2.i -> Class variables -> specific to instance of the class
+		//v.i, v1.i, v2.i -> Instance variables -> specific to instance of the class
+		System.out.println(v2.sint);
 	}
 }
