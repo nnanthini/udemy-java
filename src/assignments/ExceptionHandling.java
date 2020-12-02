c302f1d9f88a5993750353c85965f6e0223e9c47
nnanthini <nanthinin92@gmail.com>
2020-12-02 13:00:20 -0600

    Java Basics

---
+++ src/assignments/ExceptionHandling.java
@@ -0,0 +1,24 @@
+package assignments;
+
+public class ExceptionHandling {
+
+	public static void main(String[] args) {
+		/*
+		 * Error - occurs in runtime, not known to compiler, 
+		 * happens mostly due to the environment in which program is run, 
+		 * recovering from error is not possible
+		 * java.lang.Error package
+		 * Ex: StackOverflowError, OutofMemoryError
+		 * Exception - occurs in runtime, but checked can be known to compiler and unchecked not known to compiler,
+		 * happens because of the program itself,
+		 * can be handled using try catch throw 
+		 * java.lang.Exception
+		 * Checked - SQLException, IOException
+		 * Unchecked - ArrayIndexOutOfBoundException, NullPointerException
+		 * 
+		 * 
+		 */
+
+	}
+
+}
